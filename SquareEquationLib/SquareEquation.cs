7e4d05fc10561c60b95ae910d6d9d3b95d7ad578
Evtushevskiy Maksim <96142467+Maksimka3168@users.noreply.github.com>
2023-06-24 23:42:56 +0300

    Update SquareEquation.cs

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,6 +4,25 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        throw new NotImplementedException();
+        double e = 1e-9;
+        if ((-e < a && a < e) || double.IsNaN(a) || double.IsNaN(b) || double.IsNaN(c) || double.IsInfinity(a) || double.IsInfinity(b) || double.IsInfinity(c))
+        {
+            throw new ArgumentException();
+        }
+        double D = b * b - 4 * a * c;
+        if (D <= -e)
+        {
+            return new double[] { };
+        }
+        else if (-e < D && D < e)
+        { 
+            return new double[] {(-b) / 2*a};
+        }
+        else{
+            double x1 = -(b + Math.Sign(b) * Math.Sqrt(D)) / 2;
+            double x2 = c / x1;
+            return new double[] {x1, x2};
+        }
     }
 }
+
