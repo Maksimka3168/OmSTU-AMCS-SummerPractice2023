55f6faac6817544306daba987238d1102a33a8ab
Maksimka3168 <gfer-6@mail.ru>
2023-07-05 11:46:49 +0000

    Add pool

--- spacebattle/spacebattle.cs
+++ spacebattle/spacebattle.cs
@@ -1,8 +1,8 @@
 ﻿namespace spacebattle;
 public class Spaceship
 {
-    private double[]? position;
-    private double[]? boostspeed;
+    private double[] position = new double[] { 0, 0 };
+    private double[] boostspeed = new double[] {1, 1};
     private bool canChange = true;
 
     public Spaceship(){}
@@ -21,6 +21,11 @@ public class Spaceship
 
     public void setChange(bool valueChange){
         this.canChange = valueChange;
+    }
+
+    public void respawnSpaceShip(){
+        this.position = new double[] { 0, 0 };
+        this.boostspeed = new double[] {1, 1};
     }
 
     public void Move(){
